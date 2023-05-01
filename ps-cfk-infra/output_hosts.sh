6780fe8f61d2661f61c0f87cc737f779142fc2f2
Dedovic, Nermin <dedovicnermin@outlook.com>
2023-05-01 15:59:20 -0500

    initial cfk-infra

--- ps-cfk-infra/output_hosts.sh
+++ ps-cfk-infra/output_hosts.sh
@@ -1,33 +1,31 @@
 #!/usr/bin/env bash
 
+NAMESPACE=$1
+SITE=$2
 
 declare -A dns
-dns[0]="pb0.plain.kafka.nermdev.io"
-dns[1]="pb1.plain.kafka.nermdev.io"
-dns[2]="pb2.plain.kafka.nermdev.io"
-dns[3]="bootstrap.plain.kafka.nermdev.io"
-#dns[4]="mb0.mtls.kafka.nermdev.io"
-#dns[5]="mb1.mtls.kafka.nermdev.io"
-#dns[6]="mb2.mtls.kafka.nermdev.io"
-#dns[7]="bootstrap.mtls.kafka.nermdev.io"
-#dns[8]="c3.kafka.nermdev.io"
+dns[0]="b0.$NAMESPACE.$SITE.confluentps.io"
+dns[1]="b1.$NAMESPACE.$SITE.confluentps.io"
+dns[2]="b2.$NAMESPACE.$SITE.confluentps.io"
+dns[3]="bootstrap.$NAMESPACE.$SITE.confluentps.io"
+
 
 i=0
 kubectl get services --all-namespaces -o json | jq -r '.items[] | .status.loadBalancer?|.ingress[]?|.ip' | while read line ; do
   echo "$line ${dns[$i]}"
   let "i+=1"
   if [ $i -ge 5 ]; then
-    echo "$line ksql.kafka.nermdev.io"
-    echo "$line sr.kafka.nermdev.io"
-    echo "$line connect.kafka.nermdev.io"
-    echo "$line ksql.kafka.nermdev.io"
-    echo "$line krp.kafka.nermdev.io"
-    echo "$line mds.kafka.nermdev.io"
-    echo "$line grafana.nermdev.io"
-    echo "$line prometheus.nermdev.io"
-    echo "$line c3.kafka.nermdev.io"
-    echo "$line kibana.kafka.nermdev.io"
-    echo "$line elastic.kafka.nermdev.io"
+    echo "$line ksql.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line sr.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line connect.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line ksql.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line krp.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line mds.$NAMESPACE.$SITE.confluentps.io"
+    echo "$line grafana.confluentps.io"
+    echo "$line prometheus.confluentps.io"
+    echo "$line c3.$NAMESPACE.$SITE.confluentps.io"
+#    echo "$line kibana.confluentps.io"
+#    echo "$line elastic.confluentps.io"
 
 
   fi
