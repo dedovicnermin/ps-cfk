86992543174a2a00375e759fb1566164a97af48a
Nermin Đedović <62862650+dedovicnermin@users.noreply.github.com>
2023-05-04 22:26:04 -0500

    Update reconcile_all_connector.sh

--- ps-cfk-app/workshop/scripts/operator/connector/reconcile_all_connector.sh
+++ ps-cfk-app/workshop/scripts/operator/connector/reconcile_all_connector.sh
@@ -5,4 +5,4 @@ env=$1
 export NAMESPACE=$env
 for conn in $(kubectl -n "$NAMESPACE" get connector --no-headers \
 -o jsonpath='{.items[*].metadata.name}'); do \
-./operator/connector/reconcile_connector.sh "$conn"; done
+./../scripts/operator/connector/reconcile_connector.sh "$conn"; done
