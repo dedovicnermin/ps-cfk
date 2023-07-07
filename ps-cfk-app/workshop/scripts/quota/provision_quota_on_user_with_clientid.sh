a75cf337c8bf2a47a8ad615644e3278ae3e91ef4
Dedovic, Nermin <dedovicnermin@outlook.com>
2023-07-06 19:43:30 -0500

    ps-cfk : refactoring

--- ps-cfk-app/workshop/scripts/quota/provision_quota_on_user_with_clientid.sh
+++ ps-cfk-app/workshop/scripts/quota/provision_quota_on_user_with_clientid.sh
@@ -4,11 +4,11 @@ env=$1
 site=$2
 user=$3
 client=$4
-rate=$5
+quota=$5
 
 kafka-configs --bootstrap-server bootstrap."$env"."$site".confluentps.io:9092 --command-config sasl_ssl.properties \
   --alter \
-  --add-config "producer_byte_rate=$rate" \
+  --add-config "$quota" \
   --entity-type users --entity-name "$user" \
   --entity-type clients --entity-name "$client"
 
